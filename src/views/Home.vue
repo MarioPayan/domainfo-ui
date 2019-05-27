eeeb8f8698741421d5fff2fd3083164388ad3a87
Mario Payan <mariopayan@alertlogic.com>
2019-05-26 19:47:03 -0500

    Fixing one of the most stupid bugs that I have done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -48,11 +48,7 @@
         <h2 v-if="backendDown">
           Panic! Looks like there is a problem in the backend side, try with
           another url, if fail again, please contact the admin
-              <h4>
-                Type a domain like "truora.com" on the left side and click the
-                search button to get and show the info
-              </h4>
-              <h6>{{ errorMessage }}</h6>
+          <h6>{{ errorMessage }}</h6>
         </h2>
       </b-col>
       <b-col cols="8">
@@ -112,10 +108,10 @@
         </b-card>
         <b-card id="textHelp" v-else img-src="../assets/domainfo.png" img-top>
           <b-card-text>
-              <h4>
-                Type a domain like "truora.com" on the left side and click the
-                search button to get and show the info
-              </h4>
+            <h4>
+              Type a domain like "truora.com" on the left side and click the
+              search button to get and show the info
+            </h4>
           </b-card-text>
         </b-card>
       </b-col>
